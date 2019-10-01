9b9440a4e51cbde5f5dcd024df388637dcb5a558
milladiz84 <milladiz84@gmail.com>
2019-10-01 12:43:22 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,82 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const rename = require("gulp-rename");
+const rigger = require("gulp-rigger");
+const uglify = require("gulp-uglify");
+const plumber = require("gulp-plumber");
+const autoprefixer = require("gulp-autoprefixer");
+const cssbeautify = require("gulp-cssbeautify");
+const cssnano = require("gulp-cssnano");
+const notify = require("gulp-notify");
+const browserSync = require('browser-sync').create();
+
+
+
+/*gulp.task('mytask', async function(){
+	//console.log('привет, я таск!');
+	return gulp.src('source-files')
+	.pipe(plugin())
+	.pipe(gulp.dest('folder'))
+});
+*/
+
+var onError = function(err) {
+        notify.onError({message: "Failed\n<%= error.message %>", sound: true})(err);
+        this.emit('end');
+}
+
+gulp.task('js', async function() {
+	return gulp.src('js/*.js')
+		.pipe(plumber({errorHandler: onError}))
+//        .pipe(plumber())
+		.pipe(rigger())
+        .pipe(gulp.dest('app/js/'))
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: ".min",
+            extname: ".js"
+        }))
+        .pipe(gulp.dest('app/js/'))
+        .pipe(browserSync.stream());
+    });
+
+
+gulp.task('sass', async function() {
+	//return gulp.src('app/sass/**/*.sass')
+	//return gulp.src('app/sass/*.sass')
+	//return gulp.src('!app/sass/main.sass') "!" - исключает этот файл, не будет обрабатыватьсыся//
+	//return gulp.src('[!app/sass/main.sass', 'app/sass/**/*.sass']) все файлы будут обрабатываться, кроме main.sass//
+	//return gulp.src('app/sass/*.+(sccs|sass)') выбираем все scss и sass файлы только в директории sass//
+	//return gulp.src('app/sass/**/*.+(sccs|sass)') выбираем все scss и sass файлы во всех поддиректориях//
+	return gulp.src('app/sass/**/*.scss')
+    .pipe(plumber({errorHandler: onError}))
+	.pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(cssbeautify())
+	.pipe(gulp.dest('app/css/'))
+    .pipe(cssnano({
+            zindex: false,
+            discardComments: {
+                removeAll: true
+            }
+        }))
+    .pipe(rename({
+            suffix: ".min",
+            extname: ".css"
+        }))
+    .pipe(gulp.dest('app/css/'))
+	.pipe(browserSync.stream());
+	});
+
+gulp.task('default', gulp.series('sass', function() {
+ 
+    browserSync.init({
+        server: "app/"
+    });
+ 
+    gulp.watch("app/sass/*.scss", gulp.series('sass'));
+    gulp.watch("bootstrap/scss/**/*.scss", gulp.series('sass'));
+    gulp.watch('app/css/*.css').on('change', browserSync.reload);
+    gulp.watch("js/*.js").on('change', gulp.series('js'));
+    gulp.watch("app/*.html").on('change', browserSync.reload);
+}));
---
+++ js/main.js
@@ -0,0 +1,10 @@
+//= ../jquery/dist/jquery.js
+//= ../popper.js/dist/umd/popper.js
+//= ../bootstrap/js/dist/util.js
+//= ../bootstrap/js/dist/tooltip.js
+//= ../bootstrap/js/dist/popover.js
+//= ../bootstrap/js/dist/tab.js
+
+
+//= custom/tooltip.js
+//= custom/popover.js
